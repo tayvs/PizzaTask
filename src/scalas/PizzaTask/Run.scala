1d909ff531b6f216aae49fc5eeaf2a6beb4192a2
tayvs <tayvs@mail.ru>
2017-02-07 00:32:08 +0200

    Fill first Slice

--- src/scalas/PizzaTask/Run.scala
+++ src/scalas/PizzaTask/Run.scala
@@ -5,7 +5,21 @@ package scalas.PizzaTask
   */
 object Run extends App {
 
-
   val pizza = new Pizza
+
+  println("Pizza")
+  pizza.printPizza
+  println("PizzaMap")
+  pizza.printPizzaMap
+
   println(pizza.findOtherIngredient(0, 0))
+  val (r, c) = pizza.findOtherIngredient(0, 0)
+  val beginPoint = pizza.Point(0, 0)
+  val endPoint = pizza.Point(r, c)
+
+  if (pizza.isEmptyRec(beginPoint, endPoint)) {
+    pizza.fillRec(beginPoint, endPoint)
+    println("PizzaMap")
+    pizza.printPizzaMap
+  }
 }
